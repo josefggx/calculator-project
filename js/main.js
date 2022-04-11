d7b4035a56b2b62a5ddab2ead8194923f126f792
josefgg <josefgg58@gmail.com>
2022-04-11 17:05:35 -0500

    Basic Functionality

--- js/main.js
+++ js/main.js
@@ -13,11 +13,55 @@ const operate = (operator, a, b) => {
         return subtractOp(a, b);
     } else if(operator === "x") {
         return multiplyOp(a, b);
-    } else if(operator === "/") {
+    } else if(operator === "&#247;") {
         return divideOp(a, b);
     } else if(operator === "%") {
         return moduleOp(a, b);
     }
 }
 
-console.log(operate("%", 10, 4));
+const mainDisplay = document.querySelector("#current-number");
+const secDisplay = document.querySelector("#last-operation")
+
+// Selecting All buttons
+const numberButtons = document.querySelectorAll('.number-btn');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        document.querySelector('span').remove()
+        mainDisplay.innerHTML = `${mainDisplay.innerHTML}${button.textContent}`;
+    });
+});
+
+let numberOne;
+let numberTwo;
+let operation;
+
+const operationButtons = document.querySelectorAll('.operation-btn');
+operationButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        numberOne = mainDisplay.innerHTML;
+        operation = button.textContent;
+        secDisplay.innerHTML = `${numberOne} ${operation}`;
+        secNumber();
+    });
+});
+
+const secNumber = () => {
+    if(secDisplay.innerHTML !== '') {
+        let numberTwo = "";
+        numberButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                numberTwo = `${numberTwo}${button.textContent}`
+                mainDisplay.innerHTML = numberTwo;
+            });
+        });
+    }
+}
+
+const btnResult = document.querySelector("#equal");
+btnResult.addEventListener('click', () => {
+    numberTwo = mainDisplay.innerHTML;
+    secDisplay.innerHTML = `${numberOne} ${operation} ${numberTwo}`;
+    const result = operate(operation, parseInt(numberOne), parseInt(numberTwo));
+    mainDisplay.innerHTML = result;
+});
